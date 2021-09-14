6cd59130fd57673451bc125deab90f40b2095a86
Jaime Villarreal <jvillarreal337@gmail.com>
2021-09-13 23:52:49 -0500

    Added registers.

---
+++ src/Registers.js
@@ -0,0 +1,47 @@
+import { LOAD_PROGRAM_ADDRESS } from "./constants/memoryConstants";
+import { NUMBER_OF_REGISTERS, STACK_DEPTH } from "./constants/registersConstants";
+
+export class Registers {
+    constructor() {
+        this.V = new Uint8Array(NUMBER_OF_REGISTERS);
+        this.I = 0;
+        this.delayTimer = 0;
+        this.soundTimer = 0;
+        // Program counter
+        this.PC = LOAD_PROGRAM_ADDRESS;
+        // Stack pointer
+        this.SP = -1;
+        this.stack = new Uint16Array(STACK_DEPTH);
+        this.reset();
+    }
+    reset() {
+        this.V.fill(0);
+        this.I = 0;
+        this.delayTimer = 0;
+        this.soundTimer = 0;
+        this.PC = LOAD_PROGRAM_ADDRESS;
+        this.SP = -1;
+        this.stack.fill(0);
+    }
+
+    stackPush(value) {
+        this.SP++;
+        this.assertStackOverflow();
+        this.stack[this.SP] = value
+    }
+
+    stackPop() {
+        const value = this.stack[this.SP];
+        this.SP--;
+        this.assertStackUnderflow();
+        return value;
+    }
+
+    assertStackUnderflow() {
+        console.assert(this.SP >= -1, 'Error: Stack underflow');
+    }
+
+    assertStackOverflow() {
+        console.assert(this.SP < STACK_DEPTH, 'Error: Stack overflow');
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import { Chip8 } from "./Chip8";
 
 const chip8 = new Chip8();
 
-// Set 26 into memory.
-chip8.memory.setMemory(0x05, 0x1a);
-const result = chip8.memory.getMemory(0x05);
+chip8.registers.stackPush(13);
+chip8.registers.stackPush(15);
+chip8.registers.stackPush(15);
+let result;
+result = chip8.registers.stackPop();
 console.log(result);
+result = chip8.registers.stackPop();
+console.log(result);
+result = chip8.registers.stackPop();
+console.log(result);
+result = chip8.registers.stackPop();
+console.log(result);
+
+// Set 26 into memory.
+// chip8.memory.setMemory(0x05, 0x1a);
+// const result = chip8.memory.getMemory(0x05);
+// console.log(result);
